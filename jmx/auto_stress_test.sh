056a7c7bbf15c45fe83c103c48637714be5bc741
Leon-judy <67861960@qq.com>
2020-09-07 11:39:01 +0800

    5 10 15

--- jmx/auto_stress_test.sh
+++ jmx/auto_stress_test.sh
@@ -12,7 +12,7 @@ export jmeter_home="/usr/local/apache-jmeter-5.1.1"
 echo "自动化压测开始"
 
 # 压测并发数列表
-thread_number_array=(10)
+thread_number_array=(5 10 15)
 for num in "${thread_number_array[@]}"
 do
     # 生成对应压测线程的jmx文件
